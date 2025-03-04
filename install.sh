66be13491d7326385f5b0288ed1307ff2bfb6e54
Alex Covington <alex@covington.tech>
2025-03-03 21:57:18 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "This script will proceed to install ADE. This could remove or overwrite existing files, such as ~/.local/scripts/, ~/.xinitrc, ~/.bashrc, and other files that can affect your system's functionality."
+
+read -r -p "Do you want to continue? (y/n) " answer
+case "$answer" in
+	[Yy]* )
+		echo "Proceeding with install..."
+		;;
+	* )
+		echo "Aborting..."
+		exit 0
+		;;
+esac
+
+if [ -e "$HOME/.local/scripts" ]; then
+	rm -rf $HOME/.local/scripts
+fi
+
+ln -s $(pwd)/src $HOME/.local/scripts
